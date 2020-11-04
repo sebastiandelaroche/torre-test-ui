10df9f3be3476722ab9a6e348a06ada492270ff5
Sebastian De La Roche <sebastian.delaroche@elasticdevteam.com>
2020-11-04 09:06:42 -0500

    feat: scaffolding

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import './App.css';
+
+const App = () => (
+  <BrowserRouter>
+    Foo
+  </BrowserRouter>
+);
+
+export default App;
